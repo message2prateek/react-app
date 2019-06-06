3c8d49b7e800812646e6b8bf3503ed2f929f5a91
Prateek Sharma <prateek_sharma@bnz.co.nz>
2019-06-06 21:12:54 +1200

    Add the base appication

---
+++ src/components/TickyTacky/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function TickyTacky() {
+  return <h1>Ticky Tacky</h1>;
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import TickyTacky from './components/TickyTacky';
+import { render } from 'react-dom';
+import { unregister } from './serviceWorker';
+
+render(<TickyTacky />, document.getElementById('root'));
+
+unregister();
